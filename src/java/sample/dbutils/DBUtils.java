f839db78c889b0a1670902c2abcc6e64e40f4f6b
Magnusjs <57452499+magnusjss@users.noreply.github.com>
2022-12-23 20:56:22 +0700

    Update DBUtils.java

--- src/java/sample/dbutils/DBUtils.java
+++ src/java/sample/dbutils/DBUtils.java
@@ -26,7 +26,7 @@ public class DBUtils {
         Connection con = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String url = "jdbc:sqlserver://localhost:1433;databaseName=FoodManagement;";
-        con = DriverManager.getConnection(url, "sa", "12345");
+        con = DriverManager.getConnection(url, "**", "*****");
         return con;
     }
 }
